c094ddae59ba07f87cd68384e7efa58a91b7abcd
Emerson Estrella <emerson.estrella@gmail.com>
2012-10-22 02:59:09 -0200

    asdf

---
+++ server-php5.3/server.php
@@ -0,0 +1,37 @@
+<?php
+/* This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://sam.zoy.org/wtfpl/COPYING for more details. */
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+require(dirname(__FILE__) . '/lib/SplClassLoader.php');
+
+require(dirname(__FILE__) . '/lib/WebSocket/Application/Application.php');
+require(dirname(__FILE__) . '/lib/WebSocket/Application/StatusApplication.php');
+require(dirname(__FILE__) . '/lib/WebSocket/Application/DemoApplication.php');
+
+require(dirname(__FILE__) . '/lib/WebSocket/Socket.php');
+require(dirname(__FILE__) . '/lib/WebSocket/Connection.php');
+require(dirname(__FILE__) . '/lib/WebSocket/Server.php');
+
+//$classLoader = new SplClassLoader('WebSocket', dirname(__FILE__) . '/lib');
+//$classLoader->register();
+
+$server = new Server('websockets.possum-cms.com', 8002, false);
+
+// server settings:
+$server->setMaxClients(100);
+$server->setCheckOrigin(false);
+$server->setAllowedOrigin('foo.lh');
+$server->setMaxConnectionsPerIp(100);
+$server->setMaxRequestsPerMinute(2000);
+
+// Hint: Status application should not be removed as it displays usefull server informations:
+$server->registerApplication('status', StatusApplication::getInstance());
+$server->registerApplication('demo', DemoApplication::getInstance());
+
+$server->run();
--- server/server.php
+++ server/server.php
@@ -1,37 +1,77 @@
 <?php
-/* This program is free software. It comes without any warranty, to
- * the extent permitted by applicable law. You can redistribute it
- * and/or modify it under the terms of the Do What The Fuck You Want
- * To Public License, Version 2, as published by Sam Hocevar. See
- * http://sam.zoy.org/wtfpl/COPYING for more details. */
+// prevent the server from timing out
+set_time_limit(0);
 
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+// include the web sockets server script (the server is started at the far bottom of this file)
+require 'class.PHPWebSocket.php';
 
-require(dirname(__FILE__) . '/lib/SplClassLoader.php');
+// when a client sends data to the server
+function wsOnMessage($clientID, $message, $messageLength, $binary) {
+	global $Server;
+	$ip = long2ip( $Server->wsClients[$clientID][6] );
 
-require(dirname(__FILE__) . '/lib/WebSocket/Application/Application.php');
-require(dirname(__FILE__) . '/lib/WebSocket/Application/StatusApplication.php');
-require(dirname(__FILE__) . '/lib/WebSocket/Application/DemoApplication.php');
+	// check if message length is 0
+	if ($messageLength == 0) {
+		$Server->wsClose($clientID);
+		return;
+	}
 
-require(dirname(__FILE__) . '/lib/WebSocket/Socket.php');
-require(dirname(__FILE__) . '/lib/WebSocket/Connection.php');
-require(dirname(__FILE__) . '/lib/WebSocket/Server.php');
+	// command die
+	preg_match_all('/\\$die:(.*)/', $message, $matches, PREG_SET_ORDER);
+	if(isset($matches[0][1])){
+		if($matches[0][1]=="now")
+			die('died!');
+	}
+	// command move
+	preg_match_all('/\\$move:(.*),(.*)/', $message, $matches, PREG_SET_ORDER);
+	if(isset($matches[0][1])&&isset($matches[0][2]))
+		$m = $clientID.$message;
+	else
+		$m = "Visitor $clientID ($ip) said \"$message\"";
 
-//$classLoader = new SplClassLoader('WebSocket', dirname(__FILE__) . '/lib');
-//$classLoader->register();
+	//The speaker is the only person in the room. Don't let them feel lonely.
+	if ( sizeof($Server->wsClients) == 1 )
+		$Server->wsSend($clientID, "There isn't anyone else in the room, but I'll still listen to you. --Your Trusty Server");
+	else
+		//Send the message to everyone but the person who said it
+		foreach ( $Server->wsClients as $id => $client )
+			if ( $id != $clientID ){
+				$Server->wsSend($id, $m);
+			}
+}
 
-$server = new Server('websockets.possum-cms.com', 8002, false);
+// when a client connects
+function wsOnOpen($clientID)
+{
+	global $Server;
+	$ip = long2ip( $Server->wsClients[$clientID][6] );
 
-// server settings:
-$server->setMaxClients(100);
-$server->setCheckOrigin(false);
-$server->setAllowedOrigin('foo.lh');
-$server->setMaxConnectionsPerIp(100);
-$server->setMaxRequestsPerMinute(2000);
+	$Server->log( "$ip ($clientID) has connected." );
 
-// Hint: Status application should not be removed as it displays usefull server informations:
-$server->registerApplication('status', StatusApplication::getInstance());
-$server->registerApplication('demo', DemoApplication::getInstance());
+	//Send a join notice to everyone but the person who joined
+	foreach ( $Server->wsClients as $id => $client )
+		if ( $id != $clientID )
+			$Server->wsSend($id, "Visitor $clientID ($ip) has joined the room.");
+}
 
-$server->run();
+// when a client closes or lost connection
+function wsOnClose($clientID, $status) {
+	global $Server;
+	$ip = long2ip( $Server->wsClients[$clientID][6] );
+
+	$Server->log( "$ip ($clientID) has disconnected." );
+
+	//Send a user left notice to everyone in the room
+	foreach ( $Server->wsClients as $id => $client )
+		$Server->wsSend($id, "Visitor $clientID ($ip) has left the room.");
+}
+
+// start the server
+$Server = new PHPWebSocket();
+$Server->bind('message', 'wsOnMessage');
+$Server->bind('open', 'wsOnOpen');
+$Server->bind('close', 'wsOnClose');
+// for other computers to connect, you will probably need to change this to your LAN IP or external IP,
+// alternatively use: gethostbyaddr(gethostbyname($_SERVER['SERVER_NAME']))
+$Server->wsStartServer('websockets.possum-cms.com', 8002);
+
